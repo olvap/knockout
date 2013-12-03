9551871c7fe7ecee6275272b4d7b697e974bb7ab
Olvap <oldani.pablo@gmail.com>
2013-12-03 18:54:34 -0300

    first commit.

---
+++ app/assets/javascripts/models/post.js.coffee
@@ -0,0 +1,3 @@
+class @Post
+  constructor: (name) ->
+    @name = name
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knockout::Application.config.secret_token = '2e961e6fcbba5855195b8f82604628e0a3f14cdcdb3f3fd5daa6b37c4d3336c3ce514c2ee2e1d8472deb7bc18668c91e378ff8c60047dc44284e7bae492c0a7e'
